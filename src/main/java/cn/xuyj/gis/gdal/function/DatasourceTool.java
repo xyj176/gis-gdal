0715469e6388fd4b5f722cacdae5582c76831397
xuyj <1768335576@qq.com>
2024-11-12 15:15:22 +0800

    feat:输出内容调整

--- src/main/java/cn/xuyj/gis/gdal/function/DatasourceTool.java
+++ src/main/java/cn/xuyj/gis/gdal/function/DatasourceTool.java
@@ -30,46 +30,58 @@ public class DatasourceTool {
         Layer layer = dataSource.GetLayer(0);
         if (layer == null)
             throw new RuntimeException("获取【" + shpName + "】图层失败！");
+
+        System.out.println("\n******************【" + shpName + "】基本信息******************");
         //几何类型：点、线、面
         String geomTypeName = ogr.GeometryTypeToName(layer.GetGeomType());
-        System.out.println("【" + shpName + "】的几何类型：" + geomTypeName);
-        //四至
-        double[] extent = layer.GetExtent();
-        System.out.println("四至范围：" + extent[0] + "," + extent[1] + "," + extent[2] + "," + extent[3]);
-        //地块数量
-        long featureCount = layer.GetFeatureCount();
-        System.out.println("【" + shpName + "】有" + featureCount + "个地块");
-        Feature feature;
-        int featureIndex = 0;
-        layer.ResetReading();
-        System.out.println("遍历地块读取FID：");
-        while ((feature = layer.GetNextFeature()) != null) {
-            featureIndex++;
-            long fid = feature.GetFID();
-            System.out.println("第【" + featureIndex + "】个地块的FID：【" + fid + "】");
-            Geometry geometry = feature.GetGeometryRef();
-            String json = geometry.ExportToJson();
-            System.out.println("第【" + featureIndex + "】个地块的GeoJson：" + json);
-        }
+        System.out.println("几何类型：" + geomTypeName);
         //坐标系
         SpatialReference spatialReference = layer.GetSpatialRef();
         String spName = spatialReference.GetName();
-        System.out.println("【" + shpName + "】坐标系：" + spName);
+        System.out.println("坐标系：" + spName);
+        //四至
+        double[] extent = layer.GetExtent();
+        System.out.println("四至范围：" + extent[0] + "," + extent[1] + "," + extent[2] + "," + extent[3]);
+
+        System.out.println("\n********************【" + shpName + "】属性表信息********************");
         //属性表
         FeatureDefn featureDefn = layer.GetLayerDefn();
         int fieldCount = featureDefn.GetFieldCount();
-        System.out.println("【" + shpName + "】有" + fieldCount + "个属性字段");
+        System.out.println("字段数量：" + fieldCount);
         for (int i = 0; i < fieldCount; i++) {
-            System.out.println("\t第【" + (i + 1) + "】个字段");
+            System.out.println("第【" + (i + 1) + "】个字段");
             FieldDefn fieldDefn = featureDefn.GetFieldDefn(i);
             String fieldName = fieldDefn.GetName();
             String fieldTypeName = fieldDefn.GetTypeName();
             int fieldLength = fieldDefn.GetWidth();
             int fieldPrecision = fieldDefn.GetPrecision();
-            System.out.println("\t字段【" + fieldName + "】");
+            System.out.println("\t字段名【" + fieldName + "】");
             System.out.println("\t字段类型：【" + fieldTypeName + "】");
             System.out.println("\t字段长度：【" + fieldLength + "】");
             System.out.println("\t字段精度(小数位数)：【" + fieldPrecision + "】");
+        }
+
+        System.out.println("\n********************【" + shpName + "】地块信息********************");
+        //地块数量
+        long featureCount = layer.GetFeatureCount();
+        System.out.println("地块数量：" + featureCount);
+        Feature feature;
+        int featureIndex = 0;
+        layer.ResetReading();
+        while ((feature = layer.GetNextFeature()) != null) {
+            featureIndex++;
+            long fid = feature.GetFID();
+            System.out.println("\t第【" + featureIndex + "】个地块的FID：【" + fid + "】");
+            for (int i = 0; i < fieldCount; i++) {
+                FieldDefn fieldDefn = featureDefn.GetFieldDefn(i);
+                String fieldName = fieldDefn.GetName();
+                String fieldValue = feature.GetFieldAsString(fieldName);
+                System.out.println("\t第【" + featureIndex + "】个地块的字段【" + fieldName + "】的值为：" + fieldValue);
+            }
+
+            Geometry geometry = feature.GetGeometryRef();
+            String json = geometry.ExportToJson();
+            System.out.println("\t第【" + featureIndex + "】个地块的GeoJson：" + json);
         }
     }
 }
